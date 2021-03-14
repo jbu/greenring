cc585eeb7023fa8f9e79593ebf46265ddbf30a8d
James Uther <jbu@users.noreply.github.com>
2021-03-14 16:51:44 +0000

    moving to rocket

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,23 @@
-FROM rust as builder
+FROM debian:buster as builder
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y \
+    musl-tools ca-certificates curl file build-essential autoconf \
+    automake autotools-dev libtool xutils-dev && \
+    rm -rf /var/lib/apt/lists/*
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
+    sh -s -- --default-toolchain nightly -y
+RUN /root/.cargo/bin/rustup target add x86_64-unknown-linux-musl && \
+    /root/.cargo/bin/rustup target add x86_64-unknown-linux-gnu && \
+    /root/.cargo/bin/rustup target add aarch64-apple-darwin && \
+    /root/.cargo/bin/rustup target add aarch64-unknown-linux-musl
 WORKDIR /usr/src/greenring
 COPY . .
-RUN cargo build --release
+RUN /root/.cargo/bin/cargo +nightly build --release
 
 FROM debian:buster-slim
 RUN mkdir /app
 WORKDIR /app
 ENV PORT=8080 RUST_LOG=info RUST_BACKTRACE=1
+ENV ROCKET_PORT=${PORT}
 COPY --from=builder /usr/src/greenring/target/release/greenring /app/greenring
 CMD ["/app/greenring"]
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,22 @@
-use env_logger;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-//use log::{ info, error };
-use std::convert::Infallible;
-use std::env;
-use std::net::SocketAddr;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    println!("Handling request");
-    Ok(Response::new("Hello, World".into()))
+#[macro_use]
+extern crate rocket;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
 }
 
-#[tokio::main]
-async fn main() {
-    env_logger::init();
+#[get("/")]
+fn index() -> String {
+    let hello = HelloTemplate { name: "world" };
+    return format!("{}", hello.render().unwrap());
+}
 
-    for (key, value) in env::vars() {
-        println!("ENV {}: {}", key, value);
-    }
-
-    let port = env::var("PORT").unwrap().parse().unwrap();
-
-    println!("Will listen on port {:?}", port);
-
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
-
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
-
-    let server = Server::bind(&addr).serve(make_svc);
-    println!("Bound");
-
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
